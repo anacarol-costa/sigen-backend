db1e07244ae0a3148cec105bf48238555773c7f3
Lucas Reis <lucaschristian10@gmail.com>
2022-01-17 10:27:28 -0300

    adicionar lib mock-server

--- src/produto/dto/produto.dto.ts
+++ src/produto/dto/produto.dto.ts
@@ -1,9 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
-<<<<<<< HEAD
 import { CategoriaDto } from "./categoria.dto";
-=======
-import { CategoriaDto } from "src/categoria/dto/categoria.dto";
->>>>>>> c848d5085cdd9e051e08584caf1b7a2a4efdff00
 import { ItensProdutoDto } from "./itens-produto.dto";
 import { UnidadeMedidaDto } from "./unidade-medida.dto";
 
