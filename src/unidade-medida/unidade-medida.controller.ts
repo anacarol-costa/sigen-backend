f27be55890af1c48ce1a965305305edfe1ac1db1
Ana Carolina da Costa <carol.costa2502@gmail.com>
2022-02-03 15:52:07 -0300

    adicionar ApiTags em todos os controllers da unidade-medida

--- src/unidade-medida/unidade-medida.controller.ts
+++ src/unidade-medida/unidade-medida.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UnidadeMedidaDto } from './dto/unidade-medida.dto';
 import { UnidadeMedidaService } from './unidade-medida.service';
 
@@ -9,26 +9,66 @@ export class UnidadeMedidaController {
   constructor(private readonly unidadeMedidaService: UnidadeMedidaService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Criar unidade de medida.' })
+  @ApiResponse({
+    status: 201,
+    description: 'Unidade de medida.',
+    type: UnidadeMedidaDto,
+  }) 
   create(@Body() createUnidadeMedidaDto: UnidadeMedidaDto) {
     return this.unidadeMedidaService.create(createUnidadeMedidaDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Listar todas as unidades de medida.' })
+  @ApiResponse({
+    status: 200,
+    description: 'Lista de unidades de medida.',
+    type: [UnidadeMedidaDto],
+  })
   findAll() {
     return this.unidadeMedidaService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Obter unidade de medida por id.' })
+  @ApiResponse({
+    status: 200,
+    description: 'Unidade de medida encontrada.',
+    type: UnidadeMedidaDto,
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Unidade de medida não encontrada.',
+  })
   findOne(@Param('id') id: string) {
     return this.unidadeMedidaService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Alterar unidade de medida.' })
+  @ApiResponse({
+    status: 204,
+    description: 'Unidade de medida atualizada.'
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Unidade de medida não encontrada.'
+  })
   update(@Param('id') id: string, @Body() updateUnidadeMedidaDto: UnidadeMedidaDto) {
     return this.unidadeMedidaService.update(+id, updateUnidadeMedidaDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Deletar unidade de medida.' })
+  @ApiResponse({
+    status: 204,
+    description: 'Unidade de medida deletada.'
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Unidade de medida não encontrada.'
+  })
   remove(@Param('id') id: string) {
     return this.unidadeMedidaService.remove(+id);
   }
