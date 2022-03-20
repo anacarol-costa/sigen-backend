65d6db4d50f9c98d00c4b344f887989388c46e81
anacarol-costa <carol.costa2502@gmail.com>
2022-03-20 10:12:42 -0300

    adicionar env.example e resolver problema de carregar variaveis de ambiente com dotenv

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,16 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-// import { jwtConstants } from './constants';
+import { jwtConstants } from './constants';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false, //jwt expirado = 401 unauthorized
-      secretOrKey: 'teste',
-      // secretOrKey: jwtConstants.secret,
+      ignoreExpiration: false,
+      secretOrKey: jwtConstants.secret,
     });
   }
 
