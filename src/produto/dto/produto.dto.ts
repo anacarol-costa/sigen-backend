476dfabf4aefd3763cdd104774ae482c6a25da96
anacarol-costa <carol.costa2502@gmail.com>
2022-05-07 12:41:57 -0300

    ajustar ProdutoDto

--- src/produto/dto/produto.dto.ts
+++ src/produto/dto/produto.dto.ts
@@ -1,39 +1,39 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { Categoria } from "src/categoria/entities/categoria.entity";
-import { UnidadeMedida } from "src/unidade-medida/entities/unidade-medida.entity";
-import { Produto } from "../entities/produto.entity";
-import { ItensProdutoDto } from "./itens-produto.dto";
+import { ApiProperty } from '@nestjs/swagger';
+import { Categoria } from 'src/categoria/entities/categoria.entity';
+import { UnidadeMedida } from 'src/unidade-medida/entities/unidade-medida.entity';
+import { Produto } from '../entities/produto.entity';
+import { ItensProdutoDto } from './itens-produto.dto';
 
 export class ProdutoDto {
   @ApiProperty()
-  id: Number;
+  id: number;
 
   @ApiProperty()
-  nome: String;
-  
-  @ApiProperty()
-  valorBase: Number;
-  
-  @ApiProperty()
-  categoriaId: number;
+  nome: string;
 
   @ApiProperty()
-  unidadeMedidaId: number;
+  valorBase: number;
+
+  @ApiProperty()
+  categoria: number;
+
+  @ApiProperty()
+  unidadeMedida: number;
 
   @ApiProperty({ type: [ItensProdutoDto] })
   itensProduto: Array<ItensProdutoDto>;
 
-
-  static fromEntity(dto: ProdutoDto, categoria: Categoria, unidadeMedida: UnidadeMedida): Produto {
-      return new Produto(
-        dto.id,
-        dto.nome,
-        dto.valorBase, 
-        categoria,
-        unidadeMedida
-      )
+  static fromEntity(
+    dto: ProdutoDto,
+    categoria: Categoria,
+    unidadeMedida: UnidadeMedida,
+  ): Produto {
+    return new Produto(
+      dto.id,
+      dto.nome,
+      dto.valorBase,
+      categoria,
+      unidadeMedida,
+    );
   }
-
-
 }
-
